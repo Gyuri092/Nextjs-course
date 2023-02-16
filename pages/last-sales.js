9e0e093b2362d5fa878dab010d500b80262ce67c
Gyuri092 <ggr0927@gmail.com>
2023-02-16 14:39:51 +0900

    complete: client side fetching & useSWR

---
+++ pages/last-sales.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export default function LastSalesPage() {
+  const [sales, setSales] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-fcc34-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       "https://nextjs-course-fcc34-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
+  //     )
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  //   if (isLoading) {
+  //     return <p>Loading...</p>;
+  //   }
+
+  //   if (!sales) {
+  //     return <p>No data yet...</p>;
+  //   }
+  if (error) {
+    return <p>Fail to load</p>;
+  }
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
